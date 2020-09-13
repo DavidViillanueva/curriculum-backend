cf17338ee6770e389e4b7a1791a86edcf3b4d33a
Ronco-note <david1997@live.com.ar>
2020-09-13 19:58:18 -0300

    json with data update

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ app.use(cors());
 const port = process.env.PORT || 8080;
 
 const personal_data = require('./JSON/personal_data.json');
-
+const education = require('./JSON/education.json');
+const contact = require('./JSON/contact.json');
 
 app.get('/',(req,res) => {
     res.send('cv endpoint');
 });
 
+
+app.get('/personal_data',(req,res) => {
+    res.json(personal_data);
+});
+
+app.get('/personal_data',(req,res) => {
+    res.json(education);
+})
+
+app.get('/contact',(req,res) => {
+    res.json(contact);
+})
 
 app.listen(port, () => [
     console.log(`Server on: ${port}`)
